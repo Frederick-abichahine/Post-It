bda66f7ff052c817591edaf143e8b5fa423b79f3
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-07 14:36:42 +0200

    Creating <> Frontend script.js

---
+++ Frontend/script.js
@@ -0,0 +1,15 @@
+// ############################################
+//      Declaring & Initializing Variables
+// ############################################
+
+const change_theme = document.querySelector('.theme_btn');
+
+// ###################
+//      Functions
+// ###################
+
+
+
+
+
+
