d263bba2be81d21fce9469d90de55661c36890cb
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-07 14:42:32 +0200

    Updating <> Frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,7 +8,21 @@ const change_theme = document.querySelector('.theme_btn');
 //      Functions
 // ###################
 
+// Displaying Starting Theme:
+// --------------------------
 
+document.onload = setStartingTheme(localStorage.getItem('theme'));
+function setStartingTheme(themeKey) {
+    if (themeKey === 'dark') {
+      document.documentElement.classList.add('night_mode');
+    } else {
+      document.documentElement.classList.remove('night_mode');
+    }
+}
+
+
+  
+  
 
 
 
