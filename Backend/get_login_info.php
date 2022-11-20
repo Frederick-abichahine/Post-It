3cb5706020efe6758a8c91db9ea8cbeb832ec4f9
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-21 00:47:45 +0200

    Updating <> Backend get_login_info.php

--- Backend/get_login_info.php
+++ Backend/get_login_info.php
@@ -1,7 +1,19 @@
 <?php
-#Steps:
-    #include connection.php
-    #check if user isset or not
-    #if isset then get the user info from database
-    #if not isset then return false
+    include("connection.php");
+    
+    if(isset($_GET['username'])) {
+
+        $username = $_GET['username'];
+        $query = $mysqli -> prepare("SELECT * FROM users WHERE username = ?");
+        $query -> bind_param("s", $username);
+        $query -> execute();
+        $result = $query -> get_result();
+        $response = [];
+        $user = $result -> fetch_assoc();
+        $response[] = $user;
+        echo json_encode($response);
+
+    } else {
+        echo "Username was not provided.";
+    }
 ?>
