e9ea18cf7ccf177fa39a77cb82e4aa1db2afff27
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-21 00:07:30 +0200

    Updating <> Frontend script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,7 +2,7 @@
 //      Declaring & Initializing Variables
 // ############################################
 
-const change_theme = document.querySelector('.theme_btn');
+const pages = {}
 
 // ###################
 //      Functions
@@ -11,24 +11,32 @@ const change_theme = document.querySelector('.theme_btn');
 // Displaying Starting Theme:
 // --------------------------
 
-document.onload = setStartingTheme(localStorage.getItem('theme'));
-function setStartingTheme(themeKey) {
-    if (themeKey === 'dark') {
-      document.documentElement.classList.add('night_mode');
-    } else {
-      document.documentElement.classList.remove('night_mode');
-    }
+pages.loaderFunction = (func_name) => {
+  eval("pages.load" + func_name + "()");
 }
 
-change_theme.addEventListener('click', () => {
+pages.loadIndex = () => {
+  const change_theme = document.querySelector('.theme_btn');
+  document.onload = setStartingTheme(localStorage.getItem('theme'));
+  function setStartingTheme(themeKey) {
+      if (themeKey === 'dark') {
+        document.documentElement.classList.add('night_mode');
+      } else {
+        document.documentElement.classList.remove('night_mode');
+      }
+  }
+  
+  change_theme.addEventListener('click', () => {
+  
+      document.documentElement.classList.toggle('night_mode'); //changing the root
+      if (document.documentElement.classList.contains('night_mode')) {
+        localStorage.setItem('theme', 'dark'); // adding dark theme to local storage
+      } else {
+        localStorage.setItem('theme', 'light'); // adding light theme to local storage
+      }
+  });
+}
 
-    document.documentElement.classList.toggle('night_mode'); //changing the root
-    if (document.documentElement.classList.contains('night_mode')) {
-      localStorage.setItem('theme', 'dark'); // adding dark theme to local storage
-    } else {
-      localStorage.setItem('theme', 'light'); // adding light theme to local storage
-    }
-});
   
   
 
