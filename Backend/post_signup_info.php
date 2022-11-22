994ec5a04761fcf64df517b0e3bc06b4f8e7e1c2
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-22 02:20:57 +0200

    Updating <> Backend post_signup_info.php

--- Backend/post_signup_info.php
+++ Backend/post_signup_info.php
@@ -3,6 +3,18 @@ include("connection.php");
 
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])){
 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    $query = $mysqli -> prepare("INSERT INTO users(username, password, email) VALUES(?, ?, ?)");
+    $query -> bind_param("sss", $username, $password, $email);
+    $query -> execute();
+
+    $response = [];
+    $response["success"] = true;
+    echo json_encode($response);
+
 } else {
     $response = [];
     $response["success"] = false;
