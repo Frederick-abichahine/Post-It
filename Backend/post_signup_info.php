91783ac69a5e01af245ebe5182bce8d666c17233
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-23 22:12:05 +0200

    Updating <> Backend post_signup_info.php

--- Backend/post_signup_info.php
+++ Backend/post_signup_info.php
@@ -1,32 +1,25 @@
 <?php
 include("connection.php");
 
-if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])){
+if(isset($_POST['username']) && $_POST['username'] != '' && isset($_POST['email']) && $_POST['email'] != '' && isset($_POST['password']) && $_POST['password'] != ''){
 
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
 
-    $query = $mysqli -> prepare("INSERT INTO users(username, password, email) VALUES(?, ?, ?)");
-    $query -> bind_param("sss", $username, $password, $email);
-    $query -> execute();
 
+    $query = $mysqli -> prepare("SELECT * FROM users WHERE username = ?");
+    $query -> bind_param("s", $username);
+    $query -> execute();
+    $result = $query -> get_result();
+    $test = $result -> fetch_assoc();
     $response = [];
-    $response["success"] = true;
+    $response[] = "inside";
     echo json_encode($response);
 
 } else {
     $response = [];
-    $response["success"] = false;
+    $response["success"] = "outside";
     echo json_encode($response);
 }
-
-
-
-
-
-
-
-
-
 ?>
